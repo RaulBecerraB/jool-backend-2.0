797fe4411668c15e9c5db2032e0129fda6d134b3
RaulBecerraB <ingenierobecerrabarcelo@gmail.com>
2025-05-02 18:53:08 -0600

    feat(database): add initial database schema and migrations

---
+++ Migrations/20250503005135_AddHashtagsTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace jool_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHashtagsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
