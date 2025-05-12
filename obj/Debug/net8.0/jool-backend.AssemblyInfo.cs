00e7e5fe2234581fdee823e62da1a8849e8df029
RaulBecerraB <ingenierobecerrabarcelo@gmail.com>
2025-05-12 06:21:13 -0600

    chore: remove WeatherForecastController
    - Delete unused WeatherForecastController.cs
    - Clean up template code from initial project setup

--- obj/Debug/net8.0/jool-backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/jool-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("jool-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+444b6808226225c119462da3b0367c519a014218")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8f68526e74dfcd88c034c2139298896c8b85cc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("jool-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("jool-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
